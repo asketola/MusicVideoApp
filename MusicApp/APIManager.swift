c9d3ed2ead6ce74e419651b086f2e072a8959216
asketola <annemarie.s.ketola@gmail.com>
2016-05-09 14:34:49 -0700

    All changes

--- MusicApp/APIManager.swift
+++ MusicApp/APIManager.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class APIManager {
+    
+    func loadData(urlString:String, completion: (result:String) -> Void) {
+        
+        // no persistant cashing
+        let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        
+        let session = NSURLSession(configuration: config)
+        let url = NSURL(string: urlString)!
+        
+        let task = session.dataTaskWithURL(url) { (data, response, error) -> Void in
+            dispatch_async(dispatch_get_main_queue()) {
+                if error != nil {
+                    completion(result: (error!.localizedDescription))
+                } else {
+                    completion(result: "NSURLSession successful")
+                    print(data)
+                }
+            }
+        }
+        task.resume()
+        
+    }
+}
