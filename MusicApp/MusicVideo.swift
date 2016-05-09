495fc082e6afa032d90e57092f3ad9900804ea52
asketola <annemarie.s.ketola@gmail.com>
2016-05-09 16:51:20 -0700

    Updated the other video properties we wanted

--- MusicApp/MusicVideo.swift
+++ MusicApp/MusicVideo.swift
@@ -15,6 +15,13 @@ class Videos {
     private var _videoName:String
     private var _videoImageUrl:String
     private var _videoUrl:String
+    private var _videoRights:String
+    private var _videoPrice:String
+    private var _videoArtist:String
+    private var _videoImid:String
+    private var _videoGenre:String
+    private var _videoLinkToiTunes:String
+    private var _videoReleaseDate:String
     
     // make getters since the variables are private
     
@@ -30,6 +37,33 @@ class Videos {
         return _videoUrl
     }
     
+    var videoRights: String {
+        return _videoRights
+    }
+    
+    var videoPrice: String {
+        return _videoPrice
+    }
+    
+    var videoArtist: String {
+        return _videoArtist
+    }
+    
+    var videoImid: String {
+        return _videoImid
+    }
+    
+    var videoGenre: String {
+        return _videoGenre
+    }
+    
+    var videoLinkToiTunes: String {
+        return _videoLinkToiTunes
+    }
+    
+    var videoReleaseDate: String {
+        return _videoReleaseDate
+    }
     
     init(data: JSONDictionary) {
         
@@ -53,13 +87,55 @@ class Videos {
         } else {
             _videoUrl = ""
         }
+        
+        // video rights
+        if let rights = data["rights"]as? JSONDictionary, videoRights = rights["label"] as? String {
+            self._videoRights = videoRights
+        } else {
+            _videoRights = ""
+        }
+        
+        // video price
+        if let price = data["im:price"] as? JSONDictionary, videoPrice = price["label"] as? String {
+            self._videoPrice = videoPrice
+        } else {
+            _videoPrice = ""
+        }
+        
+        // video Artist
+        if let artist = data["im:artist"] as? JSONDictionary, videoArtist = artist["label"] as? String {
+            self._videoArtist = videoArtist
+        } else {
+            _videoArtist = ""
+        }
+        
+        // video Imid
+        if let imid = data["id"] as? JSONDictionary, imidAttributes = imid["attributes"] as? JSONDictionary, videoImid = imidAttributes["im:id"] as? String {
+            self._videoImid = videoImid
+        } else {
+            _videoImid = ""
+        }
+        
+        // video genre
+        if let genre = data["category"] as? JSONDictionary, videoTerm = genre["attributes"] as? JSONDictionary, videoGenre = videoTerm["term"] as? String {
+            self._videoGenre = videoGenre
+        } else {
+            _videoGenre = ""
+        }
+        
+        // video Link to iTunes
+        if let linkToiTunes = data["id"] as? JSONDictionary, videoLinkToiTunes = linkToiTunes["label"] as? String {
+            self._videoLinkToiTunes = videoLinkToiTunes
+        } else {
+            _videoLinkToiTunes = ""
+        }
+        
+        // video release date
+        if let releaseDate = data["im:releaseDate"] as? JSONDictionary, releaseDateAttributes = releaseDate["attributes"] as? JSONDictionary, videoReleaseDate = releaseDateAttributes["label"] as? String {
+            self._videoReleaseDate = videoReleaseDate
+        } else {
+            _videoReleaseDate = ""
+        }
     }
-    
-    
-    
-    
-    
-    
-    
     
 } // closes Video
