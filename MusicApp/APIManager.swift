5a052ab03c26d551867af9952133760239e29988
asketola <annemarie.s.ketola@gmail.com>
2016-05-09 15:15:28 -0700

    Added JSON Serialization to the model

--- MusicApp/APIManager.swift
+++ MusicApp/APIManager.swift
@@ -19,17 +19,34 @@ class APIManager {
         let url = NSURL(string: urlString)!
         
         let task = session.dataTaskWithURL(url) { (data, response, error) -> Void in
-            dispatch_async(dispatch_get_main_queue()) {
+           
                 if error != nil {
+                    dispatch_async(dispatch_get_main_queue()) {
                     completion(result: (error!.localizedDescription))
+                    }
                 } else {
-                    completion(result: "NSURLSession successful")
-                    print(data)
-                }
+                    
+                    do {
+                        //* .AllowFragmentation - top level object is not array or Dictionary
+                        // NSJSONSerialization requires Do/Try/Catch converts from NSData to JSON Object and casts it into a dictionary
+                        
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject] {
+                            print(json)
+                            
+                            let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                            dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                                dispatch_async(dispatch_get_main_queue()) {
+                                    completion(result: "JSONSerialization Successful")
+                                }
+                            }
+                        }
+                    } catch {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                completion(result: "error in NSJSONSerialization")
+                            }
+                        } // End of JSONSerialization
             }
-        }
-        task.resume()
-        // stuff
-        
-    }
-}
+        } // closes task
+        task.resume() // this has to be outside the task {}
+  } // closes loadData function
+} // closes APIManager class
