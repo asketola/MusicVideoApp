20b3cd675f5eb95e33c58d435368349455e35ff6
asketola <annemarie.s.ketola@gmail.com>
2016-05-09 10:53:36 -0700

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Annemarie Ketola on 5/9/16.
+//  Copyright © 2016 Annemarie Ketola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
